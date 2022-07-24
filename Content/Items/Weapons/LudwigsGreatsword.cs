159b7e50f18df9843f4563be78eeb06d9a477572
stcarda <seancarda@msn.com>
2022-07-24 01:08:23 -0700

    Updated Blood Greatsword files.

---
+++ Content/Items/Weapons/LudwigsGreatsword.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using FirstMod.Content.Projectiles;
+
+// This is a weapon from the mod FirstMod.
+namespace FirstMod.Content.Items.Weapons
+{
+    class LudwigsGreatsword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Set the name of the item that is seen in-game.
+            DisplayName.SetDefault("True Daemon's Blood Greatsword");
+
+            // Provide a short description of the item.
+            Tooltip.SetDefault("Blood-blessed Daemon's Blood Greatsword, true bane of Ludwig's faith...");
+        }
+        public override void SetDefaults()
+        {
+            // Set the weapon behavior properties.
+            Item.damage = 100;
+            Item.knockBack = 6;
+            Item.DamageType = DamageClass.Melee;
+
+            // Set the hold style.
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shoot = ModContent.ProjectileType<LudwigProj>();
+
+            // Set the properties of the animation.
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+
+            // Hitbox properties.
+            Item.height = 5;
+            Item.width = 5;
+            Item.autoReuse = true;
+        }
+
+        public override bool Shoot(
+            Player player, 
+            EntitySource_ItemUse_WithAmmo source, 
+            Vector2 position, 
+            Vector2 velocity, 
+            int type, 
+            int damage, 
+            float knockback)
+        {
+            position += new Vector2(velocity.X, velocity.Y);
+            Projectile.NewProjectile(
+                Terraria.Entity.GetSource_NaturalSpawn(),
+                position.X,
+                position.Y,
+                velocity.X / 30,
+                velocity.Y / 30,
+                type,
+                damage,
+                (int)knockback,
+                Main.myPlayer,
+                velocity.X,
+                velocity.Y
+            );
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/LudwigProj.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FirstMod.Content.Projectiles
+{
+    class LudwigProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("True Daemon's Blood Greatsword");
+            Main.projFrames[Projectile.type] = 6;
+        }
+
+        public override void SetDefaults()
+        {
+            // Set projectile hitbox params.
+            Projectile.width = 100;
+            Projectile.height = 100;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+
+            // Friendly means that the projectile will hit enemies.
+            Projectile.friendly = true;
+
+            // Call what type of projectile this is from the DamageClass class.
+            Projectile.DamageType = DamageClass.Melee;
+
+            // Hostile is by default false, but we will set it here.
+            Projectile.hostile = false;
+
+            // We may hit 3 enemies before the projectile is destroyed.
+            Projectile.penetrate = 3;
+
+            // Projectile animation params.
+            Projectile.timeLeft = 15;
+        }
+
+        public override void AI()
+        {
+            // Update the frame of the projectile based on the counter.
+            Projectile.frameCounter++; 
+            if (Projectile.frameCounter >=  5)
+            {
+                Projectile.frame++;
+                Projectile.frameCounter = 0;
+            }
+
+            // Get the angle of the projectile.
+            Vector2 angle = new Vector2(Projectile.ai[0], Projectile.ai[1]);
+            Projectile.rotation = angle.ToRotation();
+
+            // Get the player from Main and set the position of the projecile to be
+            // the center of the player.
+            Player player = Main.player[Projectile.owner];
+            Projectile.position = player.Center + angle - new Vector2(
+                Projectile.width / 2,
+                Projectile.height / 2
+            );
+        }
+    }
+}
